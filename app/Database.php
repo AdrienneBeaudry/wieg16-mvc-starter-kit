5ec95e91406d0edf4c1ba8218ec1ebaa3b9ce662
AdrienneBeaudry <adrienne.beaudry@gmail.com>
2017-05-16 10:50:16 +0200

    File and comments clean up

--- app/Database.php
+++ app/Database.php
@@ -47,59 +47,32 @@ class Database {
 
     public function create($table, $data) {
         $columns = array_keys($data);
-
         $columnSql = implode(',', $columns);
-        //'category,composition,pattern_id,fabric_date_added';
-
         $bindingSql = ':'.implode(', :', $columns);
-        //':fabric_img_url, :category, :composition, :pattern_id, :fabric_date_added';
-
-        //$sql = 'INSERT INTO `' . $table .'` (`' . $columnSql . '`) VALUES (' . $bindingSql . ')';
-
         $sql = "INSERT INTO $table ($columnSql) VALUES ($bindingSql)";
         $stm = $this->pdo->prepare($sql);
-
-        // INSERT INTO `fabrics` (`id`, `category`, `composition`, `pattern_id`, `fabric_img_url`) VALUES (NULL, 'FSD', 'DSJHFDS', 'FHDS', 'FSDFDKJH');
-        //$sql = $db->prepare("INSERT INTO db_fruit (id, type, colour) VALUES (:id, :name, :color)");
-       // $sql->execute(array('id' => $newId, 'name' => $name, 'color' => $color));
-
-
         foreach ($data as $key => $value) {
             $stm->bindValue(':'.$key, $value);
         }
         $status = $stm->execute();
-
         return ($status) ? $this->pdo->lastInsertId() : false;
     }
 
 
     public function update($table, $id, $data) {
         $columns = array_keys($data);
-
-        //$keys före
-        // ['name', 'description']
         $columns = array_map(function($item) {
             return $item . '=:' . $item;
         }, $columns);
-
-        //$columns efter
-        //['name=:name', etc]
-        //implode: 'name=:name,description
-
         $bindingSql= implode(',', $columns);
-
         $sql = "UPDATE $table SET $bindingSql WHERE id=:id";
-
         $stm = $this->pdo->prepare($sql);
-
         $data['id'] = $id;
-
         foreach ($data as $key => $value) {
             $stm->bindValue(':'.$key, $value);
         }
         $status = $stm->execute();
         return $status;
-
     }
 
 
